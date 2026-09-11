2d3e901d18f5ee61a0106d08cd00323933349db1
GUILHERME FERREIRA OLIVEIRA <46115856892@info.local>
2026-09-11 17:22:35 -0300

    :white_check_mark: Responsividade concluída.

---
+++ home/home.js
@@ -0,0 +1,13 @@
+const menu = document.getElementById("menu"); // pegando elemento pelo id
+
+// pegando elemento de forma genérica - sendo classe, utilizar o ponto '.' - ID com '#'
+const navMenu = document.querySelector(".navegacao"); 
+
+menu.addEventListener ("click", function() {
+    if(navMenu.className == "navegacao") {
+        navMenu.className = "navegacao ativo";
+    }
+    else {
+        navMenu.className = "navegacao";
+    }
+})
